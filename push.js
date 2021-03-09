8d9826a9e17297ed8d3f7c3c7839aa107d57190e
kostya vlasov <flockast@gmail.com>
2021-03-10 01:44:51 +0300

    fix

--- push.js
+++ push.js
@@ -8,7 +8,12 @@ let vapidKeys = {
 push.setVapidDetails('mailto:flockast@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
 let sub = {
-
+  endpoint: "https://fcm.googleapis.com/fcm/send/dOc0vM_7UWU:APA91bHzVmtU2gPXLWt-nle_XTYoqhh81aeQPcAncfitl1R2MKC9a6cjaPJ09CqzGcPr2yqO3-Hq_FryHQdjw7JA8l-wRns3n_LVfzlwDfyqIQHhLuTHbWAyecOABNkqkx7e-c8ZjUjS",
+  expirationTime :null,
+  keys: {
+    p256dh: "BEnj11RGHG7XFz-yiijUw-GKMLniAia0K6f1_mFptWpe7O7rcyz3dqq3KD6NAwAFeqJZRDs6KM9U7gU59MVnnZ4",
+    auth: "RvcTohO497Em6_smVbGSjA"
+  }
 }
 
 push.sendNotification(sub, 'test message')
